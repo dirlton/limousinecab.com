4d9fa9e1d36f666b8cb8864ddfdeb3420cf35743
unknown <dirlton>
2021-11-08 11:27:38 +0800

    Testing

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,9 @@ export default {
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-
+  router: {
+    base: '/the-lilium',
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'The Lilium',
