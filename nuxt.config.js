285252cd3fa4d9b2b1ef24041a1fc9e238431d10
dirlton <saltondirlys98@gmail.com>
2022-03-17 10:47:51 +0800

    Edited Header and Footer v.2

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
    router: {
-    base: '/limousine-cab',
+    base: '/',
   }, 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
