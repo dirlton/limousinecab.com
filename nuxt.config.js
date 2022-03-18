cb9acd6d8612be6eb4c0b50ed41a918867885c81
dirlton <saltondirlys98@gmail.com>
2022-03-18 16:58:18 +0800

    Added new setting v1

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
    router: {
-    base: '/',
+    base: '/limousinecab',
   }, 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
